8585206f6d6f61d8bdef4bf6c298f9cc0845f7df
EnzoFerreira <enzoraci21@gmail.com>
2023-06-07 17:20:57 -0300

    corrigido caminho errado do projeto

--- src/test/java/br/com/fiap/challenge/ChallengeApplicationTests.java
+++ src/test/java/br/com/fiap/challenge/ChallengeApplicationTests.java
@@ -3,6 +3,8 @@ package br.com.fiap.challenge;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import br.com.fiap.global.GlobalApplication;
+
 @SpringBootTest(classes = GlobalApplication.class)
 public class ChallengeApplicationTests {
 
